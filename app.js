3b08a08e5238b7f686ddce8d5995edbb8ea3a86d
lemon <eunbiryung@gmail.com>
2020-12-27 17:25:39 +0900

    user routes부분 모듈화 하기

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ** DATABASE 관련부분만 모듈화시켜서 빼는 실습 ** //
+// ** routes 관련부분만 모듈화시켜서 빼는 실습 ** //
 
 // Mongodb와 express 연결시키기 위한 기초작업
 var express = require('express');
@@ -13,6 +13,9 @@ var expressSession = require('express-session');
 // 에러 핸들러 모듈 사용
 var expressErrorHandler = require('express-error-handler');
 
+// 사용자 라우트 모듈 불러오기
+var user = require('./routes/user');
+
 // 암호화 모듈 사용
 var crypto = require('crypto');
 
@@ -21,6 +24,21 @@ var mongoose = require('mongoose')
 
 var database;
 var UserSchema;
+
+var app = express();
+
+app.set('port', process.env.port || 3000);
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(expressSession({
+    secret : 'my key',
+    resave : true,
+    saveUninitialized : true
+}));
 
 function connectDB() {
     var databaseUrl = 'mongodb://127.0.0.1:27017/local';
@@ -40,6 +58,8 @@ function connectDB() {
     });
 
     database.on('error', console.error.bind(console, 'mongoose 연결 에러'));
+
+    app.set('database', database)
    
 }
 
@@ -51,189 +71,15 @@ function createUserSchema(database) {
 
 }
 
-var app = express();
-
-app.set('port', process.env.port || 3000);
-app.use('/public', static(path.join(__dirname, 'public')));
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.use(cookieParser());
-app.use(expressSession({
-    secret : 'my key',
-    resave : true,
-    saveUninitialized : true
-}));
-
 var router = express.Router();
 
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login 라우팅 함수 호출됨')
+router.route('/process/login').post(user.login);
 
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+router.route('/process/adduser').post(user.adduser);
 
-    if(database) {
-        authUser(database, paramId, paramPassword, function(err, docs){
-            if(err) {
-                console.log('에러 발생');
-                res.writeHead(200, {"Content-type":"text/html;charset=utf-8"})
-                res.write('<h1>에러 발생</h1>')
-                res.end();
-                return
-            }
-            if(docs) {
-                console.dir(docs);
-                res.writeHead(200, {"Content-type":"text/html;charset=utf-8"})
-                res.write('<h1>사용자 로그인 성공</h1>')
-                res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>');
-                res.write('<br><br><a href="/public/login.html">다시 로그인 하기</a>');
-                res.end();
-
-            } else {
-                console.log('에러 발생');
-                res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-                res.write('<h1>사용자 데이터 조회 안됨</h1>');
-                res.end();
-            }
-        })
-    } else {
-        console.log('데이터베이스 연결 안됨');
-        res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-        res.write('<h1>데이터베이스 연결 안됨</h1>');
-        res.end();
-    }
-});
-
-router.route('/process/adduser').post(function(req, res){
-    console.log('/process/adduser 사용자 추가 함수가 호출됨');
-
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-    var paramName = req.body.name || req.query.name;
-
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
-
-    if(database) { 
-        addUser(database, paramId, paramPassword, paramName, 
-            function(err, result) {
-                if (err) {
-                    console.log('에러 발생');
-                    res.writeHead(200, {"Content-type":"text/html;charset=utf-8"})
-                    res.write('<h1>에러 발생</h1>')
-                    res.end();
-                    return;
-                }
-                if (result) {
-                    console.dir(result);
-                    res.writeHead(200, {"Content-type":"text/html;charset=utf-8"})
-                    res.write('<h1>사용자 추가 성공</h1>')
-                    res.write('<div><p>사용자 : ' + paramName + '</p></div>');
-                    res.end();
-                } else {
-                    console.log('사용자 추가 안됨');
-                    res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-                    res.write('<h1>사용자 추가 안됨</h1>');
-                    res.end();
-                }
-            })
-    } else {
-        console.log('데이터베이스 연결 안됨');
-        res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-        res.write('<h1>데이터베이스 연결 안됨</h1>');
-        res.end();
-    }
-})
-
-router.route('/process/listuser').post(function(req, res) {
-    console.log('/process/listuser 라우팅 함수 호출됨');
-
-    if(database) {
-        UserModel.findAll(function(err, results){
-            if(err) {
-                res.writeHead(200, {"Content-type":"text/html;charset=utf-8"})
-                res.write('<h1>에러 발생</h1>')
-                res.end();
-                return;
-            }
-            if(results) {
-                console.dir(results);
-                res.writeHead(200, {"Content-type":"text/html;charset=utf8"});
-                res.write("<h3>사용자 리스트</h3>");
-                res.write("<div><ul>")
-
-                for (var i = 0; i < results.length; i++ ) {
-                    var curId = results[i]._doc.id;
-                    var curName = results[i]._doc.name;
-                    res.write("    <li>#" + i + " -> " + curId + ", " + curName + "</li>");
-                    res.write("</ul></div>");
-                    res.end();
-                }
-
-            } else {
-                console.log('에러 발생');
-                res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-                res.write('<h1>조회된 사용자 없음</h1>');
-                res.end();
-            }
-        });
-    } else {
-        console.log('데이터베이스 연결 안됨');
-        res.writeHead(200, {"Content-type":"text/html;charset=utf-8"});
-        res.write('<h1>데이터베이스 연결 안됨</h1>');
-        res.end();
-    }
-})
-
+router.route('/process/listuser').post(user.listuser);
 
 app.use('/', router);
-
-var authUser = function(db, id, password, callback) {
-    console.log('authUser 호출됨' + id + ',' + password);
-
-    UserModel.findById(id, function(err, results){
-        if(err) {
-            callback(err, null);
-            return;
-        }
-        console.log('아이디 %s로 검색됨');
-        
-        if (results.length > 0 ) {
-            var user = new UserModel({id:id});
-            var authenticated = user.authenticate(password, results[0]._doc.salt, results[0]._doc.hashed_password);
-            
-            if(authenticated) {    
-                console.log('비밀번호 일치함');
-                callback(null, results);
-            } else {
-                console.log('비밀번호 불일치');
-                callback(null, null);
-            }
-
-        } else {
-            console.log('아이디 일치하는 사용자 없음')
-        }
-    });  
-};
-
-var addUser = function(db, id, password, name, callback) {
-
-    console.log('addUser함수 호출됨 : ' + id + ',' + password + ',' + name);
-
-    var user = new UserModel({"id":id, "password":password, "name":name});
-    
-    user.save(function(err){
-        if(err) {
-            callback(err, null);
-            return;
-        }
-        console.log('사용자 데이터 추가함');
-        callback(null, user);
-    });
-};
-
 
 var errorHandler = expressErrorHandler({
     static : {
